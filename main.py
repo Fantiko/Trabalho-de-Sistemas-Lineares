9c7dd701ce11d34eefe90528a6925c452f0d6b72
Fantiko <86378890+Fantiko@users.noreply.github.com>
2023-05-22 22:14:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+def lerarquivo(nomearquivo="input.txt"):
+    with open(nomearquivo) as arquivo:
+        numeros = arquivo.readline().split()
+        dicionario = {
+            "quantidade_sistemas": int(numeros[0]),
+            "dimensao": int(numeros[1]),
+            "precisao": float(numeros[2])
+        }
+        
 def main():
-
+    lerarquivo()
 
 
 if __name__ == "__main__":
