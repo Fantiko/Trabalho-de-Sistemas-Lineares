e11529483a8875b758bdff5414411f9b5139bc21
lapaCaio <lapacaio2001@gmail.com>
2023-05-24 02:52:40 -0300

    UPD - gauss_jacobi.py
    funcionando perfeitamente...

--- gauss_jacobi.py
+++ gauss_jacobi.py
@@ -28,41 +28,58 @@ def gauss_jacobi(dicionario, lista_sistemas, lista_sistemas_b):
         condicao_de_parada = True
 
         x = []
+        x_resposta = [0] * dicionario['dimensao']
         # cálculo do vetor G
         g = []
         for aux in n:
             g.append(lista_b[aux] / a[aux][aux])  # termo independente (diagonal principal) pelo pivô (B)
-        print('g:', g)
+        # print('g:', g)
         iteracao = 1
         while condicao_de_parada:
+
             if iteracao == 1:
-                x = x_anterior = g  # problema no código aqui!!! é definido que na primeira iteração o x = x_anterior = g... (linha 54)
+                x = g  # problema no código aqui!!! é definido que na primeira iteração o x = x_anterior = g... (linha 54)
             else:
-                x_anterior = x
+                x = x_resposta
+                x_resposta = [0] * dicionario['dimensao']
+
             print('iteração:', iteracao)
             # gerando a matriz para o cálculo iterativo
+            print(x)
             for i in n:
+
                 soma_com_variaveis = 0
                 for j in n:
                     if i == j:
                         continue
-                    soma_com_variaveis += (-(a[i][j] * x[j]))
-                x[i] = (lista_b[i] - soma_com_variaveis) / a[i][i]
+                    soma_com_variaveis = soma_com_variaveis + ((-1 * a[i][j]) * x[j])  # ok
+                    print(a[i][j], ' x ', x[j])
+                    print(repr(soma_com_variaveis))
 
-
+                x_resposta[i] = (lista_b[i] + soma_com_variaveis) / a[i][
+                    i]  # O MENOS NO LUGAR DO MAIS QUE ESTAVA ERRADO
+                print('valor de x apos for:', x_resposta[i])
+                print('-----------------')
             # avaliando critério de parada
-            resultado = [abs(x - y) for x, y in zip(x, x_anterior)]  # ... assim, quando chegamos aqui ainda na primeira iteração, os valores sao subtraidos, ou seja, um valor x - x, ou seja, 0 (linha 62)
+            resultado = [abs(x - y) for x, y in zip(x_resposta, x)]  # ... assim, quando chegamos aqui ainda na
+            # primeira iteração, os valores sao subtraidos, ou seja, um valor x - x, ou seja, 0 (linha 62)
 
+            print('x resposta:', x_resposta)
             print('x:', x)
-            print('x_anterior:', x_anterior)
+            print('resultado:', resultado)
 
             d = max(resultado)
-            dr = d / max(x)
+            print('d:', d)
+            dr = d / max(x_resposta)
+            print("max x:", max(x_resposta))
+            print('dr:', repr(dr))
 
-            if dr < dicionario['precisao']:  #  por fim, o valor de dr será 0, assim, sendo menor que o valor da precisao, parando o while com apenas uma iteração
-                print('dr:', repr(dr))
+            if dr < dicionario['precisao']:  # por fim, o valor de dr será 0, assim, sendo menor que o valor da
+                # precisao, parando o while com apenas uma iteração
                 condicao_de_parada = False
-                lista_resultados.append(x)
+                lista_resultados.append(x_resposta)
+            else:
+                x = x_resposta
 
             iteracao = iteracao + 1
 
