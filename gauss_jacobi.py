4b79917aa854772d6f6bcf7803428eeccc6b0ed0
lapaCaio <lapacaio2001@gmail.com>
2023-05-24 01:27:33 -0300

    UPD

--- gauss_jacobi.py
+++ gauss_jacobi.py
@@ -7,10 +7,10 @@ def ler_arquivo(nome_arquivo="input.txt"):
             "precisao": float(numeros[2])
         }
         lista_sistemas = []
-        sistema = []
         lista_sistemas_b = []
-        for sistemas in range(dicionario["quantidade_sistemas"]):
-            for iterador in range(dicionario["dimensao"]):
+        for _ in range(dicionario["quantidade_sistemas"]):
+            sistema = []
+            for _ in range(dicionario["dimensao"]):
                 linha = list(map(float, arquivo.readline().split()))
                 sistema.append(linha)
             lista_sistemas.append(sistema)
@@ -22,46 +22,58 @@ def ler_arquivo(nome_arquivo="input.txt"):
 def gauss_jacobi(dicionario, lista_sistemas, lista_sistemas_b):
     n = range(dicionario['dimensao'])
     lista_resultados = []
-
+    idx = 0
     for a in lista_sistemas:
+        lista_b = lista_sistemas_b[idx]
         condicao_de_parada = True
-        x = []
 
-        # calulo do vetor G
+        x = []
+        # cálculo do vetor G
         g = []
         for aux in n:
-            g.append(float(lista_sistemas_b[aux]) / a[aux][aux])  # termo independente(diagonal principal) pelo pivô(B)
-
+            g.append(lista_b[aux] / a[aux][aux])  # termo independente (diagonal principal) pelo pivô (B)
+        print('g:', g)
         iteracao = 1
         while condicao_de_parada:
-
             if iteracao == 1:
-                x, x_anterior = g
+                x = x_anterior = g  # problema no código aqui!!! é definido que na primeira iteração o x = x_anterior = g... (linha 54)
             else:
                 x_anterior = x
-            # gerando a matriz para o cálculo iterativo (achar alguma forma de usar isso no código do pedro)
+            print('iteração:', iteracao)
+            # gerando a matriz para o cálculo iterativo
             for i in n:
                 soma_com_variaveis = 0
                 for j in n:
                     if i == j:
                         continue
-                    soma_com_variaveis += (-1 * (a[i][j] * x[j]))
-                x[i] = (lista_sistemas_b[i] - soma_com_variaveis) / a[i][i]
+                    soma_com_variaveis += (-(a[i][j] * x[j]))
+                x[i] = (lista_b[i] - soma_com_variaveis) / a[i][i]
+
 
             # avaliando critério de parada
-            resultado = [abs(x - y) for x, y in
-                         zip(x, x_anterior)]  # subtraindo os valores da lista x_anterior dos valores da lista x
+            resultado = [abs(x - y) for x, y in zip(x, x_anterior)]  # ... assim, quando chegamos aqui ainda na primeira iteração, os valores sao subtraidos, ou seja, um valor x - x, ou seja, 0 (linha 62)
+
+            print('x:', x)
+            print('x_anterior:', x_anterior)
+
             d = max(resultado)
             dr = d / max(x)
 
-            if dr < dicionario['precisao']:
+            if dr < dicionario['precisao']:  #  por fim, o valor de dr será 0, assim, sendo menor que o valor da precisao, parando o while com apenas uma iteração
+                print('dr:', repr(dr))
                 condicao_de_parada = False
+                lista_resultados.append(x)
 
-            iteracao += 1
+            iteracao = iteracao + 1
+
+        idx += 1
+
+    return lista_resultados
 
 
 def main():
-    gauss_jacobi(*ler_arquivo())
+    r = gauss_jacobi(*ler_arquivo())
+    print(r)
 
 
 if __name__ == "__main__":
