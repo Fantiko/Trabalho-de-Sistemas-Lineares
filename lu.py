6496925ad23569788a6f57b1fcd64e2491d4025f
Rok <rexblanegamer@gmail.com>
2023-05-27 03:13:12 -0300

    Traduzi o árabe do tiago

--- lu.py
+++ lu.py
@@ -1,96 +1,97 @@
-class Lu:
-    def __init__(self, size):
-        self.size = size
-        self.sistema = np.zeros((size, size))
-        self.matrizL = np.zeros((size, size))
-        self.matrizU = np.zeros((size, size))
-        self.arrayY = np.zeros(size)
-        self.arrayR = np.zeros(size)
-        self.arrayX = np.zeros(size)
+import time
 
-    def ImprimeMatriz(self, matriz):
-        for i in range(self.size):
-            print()
-            for j in range(self.size):
-                print(f'{matriz[i][j]:.2f}', end=' ')
+def ImprimeMatriz(matriz, size):
+    for i in range(size):
         print()
+        for j in range(size):
+            print(f'{matriz[i][j]:.2f}', end=' ')
+    print()
 
-    def ImprimeVetor(self, vetor):
-        for i in range(self.size):
-            print(f'{vetor[i]:.2f}')
-        print()
+def ImprimeVetor(vetor, size):
+    for i in range(size):
+        print(f'{vetor[i]:.2f}')
+    print()
 
-    def OperacaoPivo(self, k):
-        for i in range(k + 1, self.size):
-            self.matrizL[i][k] = self.sistema[i][k] / self.sistema[k][k]
-            for j in range(k, self.size):
-                self.sistema[i][j] -= self.matrizL[i][k] * self.sistema[k][j]
+def OperacaoPivo(sistema, matrizL, size, k):
+    for i in range(k + 1, size):
+        matrizL[i][k] = sistema[i][k] / sistema[k][k]
+        for j in range(k, size):
+            sistema[i][j] -= matrizL[i][k] * sistema[k][j]
 
-    def MatrizL(self):
-        for i in range(self.size):
-            self.matrizL[i][i] = 1.0
-            for j in range(i + 1, self.size):
-                self.matrizL[i][j] = 0.0
+def MatrizL(matrizL, size):
+    for i in range(size):
+        matrizL[i][i] = 1.0
+        for j in range(i + 1, size):
+            matrizL[i][j] = 0.0
 
-    def MatrizU(self):
-        for i in range(self.size):
-            for j in range(i, self.size):
-                self.matrizU[i][j] = self.sistema[i][j]
+def MatrizU(sistema, matrizU, size):
+    for i in range(size):
+        for j in range(i, size):
+            matrizU[i][j] = sistema[i][j]
 
-    def LY(self):
-        for i in range(self.size):
-            self.arrayY[i] = self.arrayR[i]
-            for j in range(i):
-                self.arrayY[i] -= self.matrizL[i][j] * self.arrayY[j]
+def LY(matrizL, arrayR, arrayY, size):
+    for i in range(size):
+        arrayY[i] = arrayR[i]
+        for j in range(i):
+            arrayY[i] -= matrizL[i][j] * arrayY[j]
 
-    def UX(self):
-        for i in range(self.size - 1, -1, -1):
-            sum = 0.0
-            for j in range(i + 1, self.size):
-                sum += self.matrizU[i][j] * self.arrayX[j]
-            self.arrayX[i] = (self.arrayY[i] - sum) / self.matrizU[i][i]
+def UX(matrizU, arrayY, arrayX, size):
+    for i in range(size - 1, -1, -1):
+        sum = 0.0
+        for j in range(i + 1, size):
+            sum += matrizU[i][j] * arrayX[j]
+        arrayX[i] = (arrayY[i] - sum) / matrizU[i][i]
 
 
-filename = "entrada.txt"
+filename = "input.txt"
 
 with open(filename, 'r') as file:
     # Leitura da quantidade de matrizes, tamanho do vetor e precisão
     line = file.readline().split()
     num_matrices = int(line[0])
     size = int(line[1])
-    matrizA = np.zeros((size, size))
+    matrizA = [[0.0] * size for _ in range(size)]
 
     # Leitura da matriz A
     for i in range(size):
         matrizA[i] = list(map(float, file.readline().split()))
 
+    tempos = []
     for _ in range(num_matrices):
-        lu = Lu(size)
-
-        # Definir a matriz A para cada iteração
-        lu.sistema = matrizA.copy()
+        inicio = time.perf_counter()
+        sistema = [row.copy() for row in matrizA]
+        matrizL = [[0.0] * size for _ in range(size)]
+        matrizU = [[0.0] * size for _ in range(size)]
+        arrayR = [0.0] * size
+        arrayY = [0.0] * size
+        arrayX = [0.0] * size
 
         # Leitura da matriz B
-        lu.arrayR = list(map(float, file.readline().split()))
+        arrayR = list(map(float, file.readline().split()))
 
         print("\nMatriz A:")
-        lu.ImprimeMatriz(lu.sistema)
+        ImprimeMatriz(sistema, size)
 
         for k in range(size - 1):
-            lu.OperacaoPivo(k)
+            OperacaoPivo(sistema, matrizL, size, k)
 
         print("\n\nMatriz L:")
-        lu.MatrizL()
-        lu.ImprimeMatriz(lu.matrizL)
+        MatrizL(matrizL, size)
+        ImprimeMatriz(matrizL, size)
 
         print("\n\nMatriz U:")
-        lu.MatrizU()
-        lu.ImprimeMatriz(lu.matrizU)
+        MatrizU(sistema, matrizU, size)
+        ImprimeMatriz(matrizU, size)
 
         print("\n\nVetor Y:")
-        lu.LY()
-        lu.ImprimeVetor(lu.arrayY)
+        LY(matrizL, arrayR, arrayY, size)
+        ImprimeVetor(arrayY, size)
 
         print("\n\nVetor X (Solução):")
-        lu.UX()
-        lu.ImprimeVetor(lu.arrayX)
+        UX(matrizU, arrayY, arrayX, size)
+        ImprimeVetor(arrayX, size)
+        fim = time.perf_counter()  # calcular o tempo
+        tempo_decorrido = fim - inicio  # calcular o tempo
+        tempos.append(tempo_decorrido)  # calcular o tempo
+
+    print(tempos)  # calcular o tempo
